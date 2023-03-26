829eac719d4662e0758ec44a9e79a7c1dbad3d2f
dryuta <tuyosanosaki@gmail.com>
2023-03-26 19:06:01 +0900

    userテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema.define(version: 2023_03_26_060318) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
