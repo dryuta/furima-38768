0731892bff7b94acb9806157f4b37c59bb6c47df
dryuta <tuyosanosaki@gmail.com>
2023-03-29 21:32:48 +0900

    全角のひらがな漢字カタカナのみにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]/
+  VALID_NAME_REGEX =   /\A[ぁ-んァ-ン一-龥]+\z/
   VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
 
   validates :nickname        , presence: true
