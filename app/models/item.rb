a70d666faae44fdaeba0139cb436b0b8f6ac5537
dryuta <tuyosanosaki@gmail.com>
2023-04-02 10:40:55 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+
+  validates :name, :description, :price, :image, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
 end
