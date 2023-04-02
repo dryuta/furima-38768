493485cdb840b37c8ffac3944a2a7cb9fa041a60
dryuta <tuyosanosaki@gmail.com>
2023-04-02 10:58:24 +0900

    itemsコントローラーを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,15 +3,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:name, :description, :category_id, :price, :,:item).permit(:image).merge(user_id: current_user.id)
   end
 end
