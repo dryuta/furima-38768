e9635edcf010e5ebbde503d0de0dbe616ba13437
dryuta <tuyosanosaki@gmail.com>
2023-04-03 13:34:41 +0900

    300円以上かつ9,999,999円以下で、半角数字でないと入力不可

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
   belongs_to :condition
 
   validates :name, :description, :price, :image, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   validates :category_id, numericality: { other_than: 1, message: "category can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "condition can't be blank" }
