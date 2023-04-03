8be3d0e3744509b568dfc1b030f67de128d11efa
dryuta <tuyosanosaki@gmail.com>
2023-04-03 15:26:45 +0900

    ここまでのテストコードはおｋ

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,12 +11,24 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
     end
+
     context '出品ができないとき' do
       it '１枚画像がないと出品できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
+      it '商品名が空欄だと出品できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品の説明が空欄だと出品できない' do
+        @item.description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      
     end
   end
 end
