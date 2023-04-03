3b4c313b899da3856401f8dd7387bcb4cd78e208
dryuta <tuyosanosaki@gmail.com>
2023-04-03 21:11:45 +0900

    不要な:itemを削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
-                                 :scheduled_delivery_id, :price, :item, :image).merge(user_id: current_user.id)
+                                 :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 end
