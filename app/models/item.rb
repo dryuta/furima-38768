4d9ed78df9df9acf920ff4535adbad4a95c2e3fa
dryuta <tuyosanosaki@gmail.com>
2023-04-03 10:37:27 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,5 +12,9 @@ class Item < ApplicationRecord
 
   validates :name, :description, :price, :image, presence: true
 
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id,  numericality: { other_than: 1, message: "category can't be blank" }
+  validates :condition_id,  numericality: { other_than: 1, message: "condition can't be blank" }
+  validates :shipping_cost_id,  numericality: { other_than: 1, message: "shipping_cost can't be blank" }
+  validates :prefecture_id,  numericality: { other_than: 1, message: "prefecture can't be blank" }
+  validates :scheduled_delivery,  numericality: { other_than: 1, message: "scheduled can't be blank" }
 end
