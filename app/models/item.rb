3b08dec61f3d5bc6974f80416d2ec5421d6f71c6
dryuta <tuyosanosaki@gmail.com>
2023-04-03 10:27:54 +0900

    itemモデルにアソシエーションを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,11 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+  belongs_to :shipping_cost
+  belongs_to :condition
+
 
   validates :name, :description, :price, :image, presence: true
 
