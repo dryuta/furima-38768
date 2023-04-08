ab45de2ad9646468607e6185d47171524b29601c
dryuta <tuyosanosaki@gmail.com>
2023-04-08 19:42:49 +0900

    2つのテーブルを作成

--- db/migrate/20230408075909_create_deliver_addresses.rb
+++ db/migrate/20230408075909_create_deliver_addresses.rb
@@ -1,6 +1,13 @@
 class CreateDeliverAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :deliver_addresses do |t|
+      t.references :order,                 null: false, foreign_key: true
+      t.string :zip_code,                  null: false
+      t.integer :prefecture_id,            null: false
+      t.string :city,                      null: false
+      t.string :address,                   null: false
+      t.string :building
+      t.string :telephone,                 null: false
 
       t.timestamps
     end
