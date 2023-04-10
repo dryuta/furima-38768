ae54559eee8ff1db375dff1458be5657dba12398
dryuta <tuyosanosaki@gmail.com>
2023-04-10 14:15:43 +0900

    コントローラーを作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,19 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
+    @order = Order.create(order_params)
+    DeliverAddress.create(deliver_address_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.merge(user_id: current_user.id, item_id: params[:item_id])
+
+  end
+
+  def deliver_address_params
+    params.permit(:zip_code, :prefecture_id, :city, :address, :building, :telephone).merge(order_id: @order.id)
   end
 end
