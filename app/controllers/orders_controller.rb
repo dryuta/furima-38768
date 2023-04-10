b24477807ea56dabf75076a13f8b77ceba4d09f0
dryuta <tuyosanosaki@gmail.com>
2023-04-10 20:09:08 +0900

    エラーが出るようにした。保存ができるようにした

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,21 @@
 class OrdersController < ApplicationController
   def index
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order = Order.create(order_params)
-    DeliverAddress.create(deliver_address_params)
-    redirect_to root_path
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+       @order_address.save
+       redirect_to root_path
+    else
+       render :index
+    end
   end
 
   private
 
   def order_params
-    params.merge(user_id: current_user.id, item_id: params[:item_id])
-
-  end
-
-  def deliver_address_params
-    params.permit(:zip_code, :prefecture_id, :city, :address, :building, :telephone).merge(order_id: @order.id)
+    params.require(:order_address).permit(:zip_code, :prefecture_id, :city, :address, :building, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,25 +1,22 @@
-class OrderForm
+class OrderAddress
   include ActiveModel::Model
-  
-  attr_accessor :user_id, :item_id, :zip_code, :prefecture_id, :city, :address, :building, :telephone
+  attr_accessor :zip_code, :prefecture_id, :city, :address, :building, :telephone, :user_id, :item_id
 
-  # 4行目と同じくこのタイミングでは生成前なので「validates :order_id」は不要
   with_options presence: true do
-    # orderモデルのバリデーション
     validates :user_id
     validates :item_id
-    # deliver_addressモデルのバリデーション
     validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :address
     validates :telephone, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
-    # トークンのバリデーション
   end
 
   def save
-    order = Order.create(user_id: user_id, item_id: item_id)
-    # ストロングパラメーターでデータが運ばれ、それらが保存のタイミングで「order_id」が生成され、保存される。
+    # 寄付情報を保存し、変数donationに代入する
+    order = Order.create(user_id: user_id, item_id:item_id)
+    # 住所を保存する
+    # donation_idには、変数donationのidと指定する
     DeliverAddress.create(order_id: order.id, zip_code: zip_code, prefecture_id: prefecture_id, city: city, address: address, building: building, telephone: telephone)
   end
 end
