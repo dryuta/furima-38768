429969f687ba0d480d5e544e017c25eb9e0ef601
dryuta <tuyosanosaki@gmail.com>
2023-04-11 12:33:49 +0900

    条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,9 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+    unless current_user != @item.user && @item.order.nil?
+      redirect_to root_path
+    end
   end
 
   def create
