40697d94629a1e5a6e3862170eac05198df32d86
dryuta <tuyosanosaki@gmail.com>
2023-04-13 12:50:33 +0900

    ?

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,27 @@
 const pay = () => {
+  const payjp = Payjp('pk_test_77dcf84d838aeb9db7bce827')
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement    = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+
   const submit = document.getElementById("button");
+
+
   if (!submit){ return false;}
   submit.addEventListener("click", (e) => {
-    console.log("フォーム送信時にイベント発火")
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
