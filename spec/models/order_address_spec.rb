c613708b051b3b02c5715a5a02debeb0ff199e7e
dryuta <tuyosanosaki@gmail.com>
2023-04-15 18:30:15 +0900

    FactoryBotに記載した外部キーを変更

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
   before do
-    @order_address = FactoryBot.build(:order_address)
+    user = FactoryBot.build(:user)
+    user.id = 1
+    item = FactoryBot.build(:item)
+    item.id = 1
+    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
   end
 
   describe '配送先情報の保存' do
