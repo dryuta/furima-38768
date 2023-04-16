3e645dd96a8d77c2dd25848055d05ea0a56665ac
dryuta <tuyosanosaki@gmail.com>
2023-04-16 09:53:16 +0900

    a

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  before_action :item_set, [:index, :create]
+  before_action :item_set, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
